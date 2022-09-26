e192c86fa8ea1ea8dc2e19110ffb729ce0808dce
ERAY <ERAY@DESKTOP-9JI43N4>
2022-09-26 16:41:31 +0300

    odev1.2.1

---
+++ odev1.2.1/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//for
+		/*
+		for(int i=1; i<=10; i++) {
+			System.out.println(i);														
+		}
+		System.out.println("döngü bitti");
+		*/
+		
+		/////////////////////////////////////////////////////////////////////
+		
+		//while
+		
+		/*int i=1;
+		while(i<=10) {									
+			System.out.println(i);
+			i++;
+		}
+		*/
+		
+		/////////////////////////////////////////////////////////////////////////
+		
+		//do-while
+		int i=1;
+		do //şartı sağlamasa da her türlü bir kere iterasyona giriyor 
+		{
+			System.out.println(i);
+			i++;
+		}
+		while(i<=10);
+
+	}
+}
